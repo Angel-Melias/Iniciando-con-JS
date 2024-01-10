2528ce5a4b33168bf873946bd6ec78eb2c582c64
Angel-Melias <martinezzelias16@gmail.com>
2024-01-09 18:46:26 -0600

    agregamos 2 funciones askNumber pregunta el numero y checkifCorrect hace la comparacion

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-alert("Hello World");
+let numero = 6;     
+let numeroElegido;
 
+function askNumber () {
+    numeroElegido = parseInt(prompt("guest the number I have on mint"));
+    checkIfCorrect();
+}
 
-let numeroelegido = prompt("guest the number I have on mint");
+function checkIfCorrect() {
 
+    if (numero == numeroElegido) {
+        alert("Eso es correcto!")
+    }
+    
+    else {
+        alert("estas bien menso ese no es");
+        askNumber();
+    }
+}
+
+askNumber();
 
 
